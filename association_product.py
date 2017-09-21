0688fbb59f420000ad06a708845fa624db8a4a93
YanasR <helloyanas@gmail.com>
2017-09-21 23:17:25 +0530

    updated association

--- association_product.py
+++ association_product.py
@@ -15,21 +15,30 @@ def Create_item_list():
 #csv_filename
     csv_filename = os.path.join(current_file, '../Data/D11-02/D01')
     Data = pd.read_csv(csv_filename,sep=';')
-
     
-    basket =Data.groupby(['cust_id','product_id'])['amount'].sum().unstack().reset_index().fillna(0).set_index('cust_id')
+    Data = Data.ix[0:1000,:]
+    Data['key'] = Data['trans_date_time'].map(str) + Data['cust_id'].map(str)
+    
+    
+    
+    basket_prep = Data.groupby(['key','product_id'])['amount'].sum()
+    
+    basket = basket_prep.unstack()
+    
+    basket = basket.fillna(0)
     
     basket_sets = basket.applymap(encode_units)
     
-    frequent_itemsets = apriori(basket_sets, min_support=0.01, use_colnames=True)
-    
-    print(frequent_itemsets)
+    print(basket_sets.ix[0])
+       
+    frequent_itemsets = apriori(basket_sets, min_support=0.07, use_colnames=True)
     
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+   
     print(rules.head())
     
     
-    #product_freq = Data[['product_id']].groupby('product_id').count()
+    #product_freq = Data[['product_id']].groupby('product_id').count() '''
     
     
     
